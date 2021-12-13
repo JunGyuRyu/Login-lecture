a6ea412d3b2a74f9e5a48a1fa090688aa08841b3
JunGyu <qurrpdnjs1@naver.com>
2021-12-13 22:56:16 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,7 +11,14 @@ function login() {
         id: id.value,
         psword: psword.value,
     };
-    console.log(req);
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body:JSON.stringify(req),
+    });
 }
 
 
