bbd64d500fc1f31c1ed42380323126ae85b38bf7
JunGyu <qurrpdnjs1@naver.com>
2021-12-13 23:38:09 +0900

    백앤드에서 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
 
 //모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 //라우팅
@@ -12,6 +13,9 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+//URL을 통해 전달되는 데이터네 한글, 공백 같은 문자가 포함되면 제대로 인식 안되는 문제 해결
+app.use(bodyParser.urlencoded({extened: true}));
 
 app.use("/",home); // use->미들웨어를 등록해주는 메서드
 
--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -18,7 +18,11 @@ function login() {
             "Content-Type": "application/json",
         },
         body:JSON.stringify(req),
-    });
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            
+        });
 }
 
 
